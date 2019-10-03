578b0f428d62875bcce27a49793b858ea4e9ceae
Alberto Aznar <albertokr@gmail.com>
2019-10-03 11:10:03 +0200

    Updated to Swift 5.0. UIButtons now are loadable

--- ContentLoader/Classes/Extensions/UIView+Loader.swift
+++ ContentLoader/Classes/Extensions/UIView+Loader.swift
@@ -9,6 +9,12 @@ enum ViewAssociatedKeys {
     static var loadable = "loadable"
     static var active = "active"
     static var loader = "loader"
+    
+    // To save values
+    static var backgroundColor = "backgroundColor"
+    static var cornerRadius = "cornerRadius"
+    static var clipToBounds = "clipToBounds"
+    static var textColor = "textColor"
 }
 
 public extension UIView {
@@ -57,9 +63,12 @@ public extension UIView {
         // Get all UIImageView
         for view in self.getAllSubviews() as [UIImageView] {
             if view.isLoadable {
+                if loading {
+                    view.clipToBounds_ = view.clipsToBounds
+                }
                 view.layer.backgroundColor = loading ? loadingFormat.color.cgColor : nil
                 view.cornerRadius = view.cornerRadius == 0 ? loadingFormat.radius : view.cornerRadius
-                view.clipsToBounds = view.cornerRadius != 0
+                view.clipsToBounds = loading ? view.cornerRadius != 0 : view.clipToBounds_
                 
                 if loading {
                     view.originalImage = view.image
@@ -75,10 +84,30 @@ public extension UIView {
         // Get all UILabel
         for view in self.getAllSubviews() as [UILabel] {
             if view.isLoadable {
+                if loading {
+                    view.textColor_ = view.textColor
+                    view.cornerRadius_ = view.cornerRadius
+                    view.clipToBounds_ = view.clipsToBounds
+                }
                 view.layer.backgroundColor = loading ? loadingFormat.color.cgColor : nil
-                view.textColor = loading ? .clear : .darkGray
-                view.cornerRadius = loadingFormat.radius
-                view.clipsToBounds = view.cornerRadius != 0
+                view.textColor = loading ? .clear : view.textColor_
+                view.cornerRadius = loading ? loadingFormat.radius : view.cornerRadius_
+                view.clipsToBounds = loading ? view.cornerRadius != 0 : view.clipToBounds_
+                
+                if loading { view.layer.playAnimation(loadingFormat.animation) }
+                else { view.layer.stopAnimation() }
+            }
+        }
+        
+        // Get all UIButton
+        for view in self.getAllSubviews() as [UIButton] {
+            if view.isLoadable {
+                if loading {
+                    view.backgroundColor_ = view.backgroundColor
+                    view.textColor_ = view.titleColor(for: .normal)
+                }
+                view.backgroundColor = loading ? loadingFormat.color : view.backgroundColor_
+                view.setTitleColor(loading ? .clear : view.textColor_, for: .normal)
                 
                 if loading { view.layer.playAnimation(loadingFormat.animation) }
                 else { view.layer.stopAnimation() }
@@ -124,5 +153,25 @@ public extension UIView {
     fileprivate var loader: ContentLoader? {
         get { return ao_get(pkey: &ViewAssociatedKeys.loader) as? ContentLoader }
         set { ao_set(newValue ?? false, pkey: &ViewAssociatedKeys.loader) }
+    }
+    
+    fileprivate var backgroundColor_: UIColor? {
+        get { return ao_get(pkey: &ViewAssociatedKeys.backgroundColor) as? UIColor }
+        set { ao_setOptional(newValue, pkey: &ViewAssociatedKeys.backgroundColor) }
+    }
+    
+    fileprivate var cornerRadius_: CGFloat! {
+        get { return ao_get(pkey: &ViewAssociatedKeys.cornerRadius) as? CGFloat ?? 0 }
+        set { ao_setOptional(newValue ?? 0, pkey: &ViewAssociatedKeys.cornerRadius) }
+    }
+    
+    fileprivate var clipToBounds_: Bool! {
+        get { return ao_get(pkey: &ViewAssociatedKeys.clipToBounds) as? Bool ?? false }
+        set { ao_setOptional(newValue ?? false, pkey: &ViewAssociatedKeys.clipToBounds) }
+    }
+    
+    fileprivate var textColor_: UIColor? {
+        get { return ao_get(pkey: &ViewAssociatedKeys.textColor) as? UIColor }
+        set { ao_setOptional(newValue, pkey: &ViewAssociatedKeys.textColor) }
     }
 }
